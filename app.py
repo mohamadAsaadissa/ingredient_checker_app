ef34555c4f7487e1e67f9df4db14c92f454a5f1b
Mohamad Issa <46658224+mohamadAsaadissa@users.noreply.github.com>
2025-05-16 20:41:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,9 @@ def get_ocr_from_camera():
     for (bbox, text, confidence) in results:
         top_left = tuple(bbox[0])
         bottom_right = tuple(bbox[2])
-        draw.rectangle([top_left, bottom_right], outline="red", width=3)
+        #draw.rectangle([top_left, bottom_right], outline="red", width=3)
+        draw.rectangle([100, 50], outline="red", width=3)
+
     
 
  return img_file
